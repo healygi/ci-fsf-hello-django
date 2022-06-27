51bb8081e50bb8b0aff4390c508f3018a5cd56fc
healygi <healygi@tcd.ie>
2022-06-27 14:41:49 +0000

    Add buttons to todo list and full CRUD functionality

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -1,24 +1,27 @@
-"""django_todo URL Configuration
+"""django_todo  Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+The `patterns` list routes s to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/s/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a  to patterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a  to patterns:  path('', Home.as_view(), name='home')
+Including another conf
+    1. Import the include() function: from django.s import include, path
+    2. Add a  to patterns:  path('blog/', include('blog.s'))
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list, add_item
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todo_list, name='get_todo_list'),
-    path('add', add_item, name='add')
+    path('', views.get_todo_list, name='get_todo_list'),
+    path('add', views.add_item, name='add'),
+    path('edit/<item_id>', views.edit_item, name='edit'),
+    path('toggle/<item_id>', views.toggle_item, name='toggle'),
+    path('delete/<item_id>', views.delete_item, name='delete'),
 ]
